8bb30a28cefeeef101d5147f9572e9e7bc609e19
tawhidabdullah <tawhidabdullah22@gmail.com>
2019-04-10 21:24:56 +0600

    footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,12 +1,80 @@
 import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <footer className="py-5 bg-dark">
-      <div className="container">
-        <p className="m-0 text-center text-white lead">
-          Copyright © Tawhid Abdullah 2019
-        </p>
+    <footer>
+      <div class="footer">
+        <div class="contain">
+          <div class="col">
+            <h1>Company</h1>
+            <ul>
+              <li>About</li>
+              <li>Mission</li>
+              <li>Services</li>
+              <li>Social</li>
+              <li>Get in touch</li>
+            </ul>
+          </div>
+          <div class="col">
+            <h1>Products</h1>
+            <ul>
+              <li>About</li>
+              <li>Mission</li>
+              <li>Services</li>
+              <li>Social</li>
+              <li>Get in touch</li>
+            </ul>
+          </div>
+          <div class="col">
+            <h1>Accounts</h1>
+            <ul>
+              <li>About</li>
+              <li>Mission</li>
+              <li>Services</li>
+              <li>Social</li>
+              <li>Get in touch</li>
+            </ul>
+          </div>
+          <div class="col">
+            <h1>Resources</h1>
+            <ul>
+              <li>Webmail</li>
+              <li>Redeem code</li>
+              <li>WHOIS lookup</li>
+              <li>Site map</li>
+              <li>Web templates</li>
+              <li>Email templates</li>
+            </ul>
+          </div>
+          <div class="col">
+            <h1>Support</h1>
+            <ul>
+              <li>Contact us</li>
+              <li>Web chat</li>
+              <li>Open ticket</li>
+            </ul>
+          </div>
+          <div class="col social">
+            <h1>Social</h1>
+            <ul>
+              <li>
+                <i className="fa fa-facebook" />
+              </li>
+              <li>
+                <i className="fa fa-instagram" />
+              </li>
+
+              <li>
+                <i className="fa fa-twiiter" />
+              </li>
+              <li>
+                <i className="fa fa-youtube" />
+              </li>
+            </ul>
+          </div>
+          <div class="clearfix" />
+        </div>
       </div>
     </footer>
   );
