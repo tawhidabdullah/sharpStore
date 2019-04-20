ddf512b576d786b1c23fdecd3a65274828e59b74
tawhidabdullah <tawhidabdullah22@gmail.com>
2019-04-20 23:15:16 +0600

    administrating..

--- Server.js
+++ Server.js
@@ -55,10 +55,9 @@ const port = process.env.PORT || 5000;
 
 // if production then server statice production
 if (process.env.NODE_ENV === "production") {
-
   // set static folder
-  app.use(express.static("client/bluild"));
-  
+  app.use(express.static("client/build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
