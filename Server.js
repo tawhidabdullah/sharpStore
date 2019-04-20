ac7a9aab9052b753d2b9d3425dca5216e0ec0983
tawhidabdullah <tawhidabdullah22@gmail.com>
2019-04-21 02:18:55 +0600

    administrating...

--- Server.js
+++ Server.js
@@ -60,9 +60,9 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
+ 
+const port = process.env.PORT || 10000;
+app.listen(port, () => {   
   console.log(
     `Tawhid Abdullah is a great programmer, server is runnig on ${port}...`
   );
